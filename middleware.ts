d96d0c030d991e1d8684346085c31dd9adbeedf9
petersahanaya <114085979+petersahanaya@users.noreply.github.com>
2023-01-17 01:23:39 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,4 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export default async function middleware (request : NextRequest) {
-    const session = request.cookies.get("next-auth.session-token")
-
-    if(!session?.value) return NextResponse.redirect(`${process.env.PORT}`)
-}
+import { default } from "next-auth/middleware"
 
 export const config = {
     matcher : ["/profile/:path*", "/user/:path*", "/comment/:path*", "/friends/:path*", "/post/:path*"]
