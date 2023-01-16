0ed24146f3b65397b9e87eebc04f5cf7ce9dc980
petersahanaya <114085979+petersahanaya@users.noreply.github.com>
2023-01-17 01:28:42 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { default } from "next-auth/middleware"
+export { default } from "next-auth/middleware"
 
 export const config = {
     matcher : ["/profile/:path*", "/user/:path*", "/comment/:path*", "/friends/:path*", "/post/:path*"]
