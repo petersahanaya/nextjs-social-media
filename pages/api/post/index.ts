53d8f0812c7fb789c54c01388dedd0cd3e53248c
petersahanaya <petersahanaya09@gmail.com>
2023-01-17 16:39:30 +0900

    Upadted.

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,8 +1,6 @@
 import { NextApiResponse } from "next";
 import nc from "next-connect";
 import { connectToDatabase } from "../../../lib/mongodb";
-import multer from "multer";
-import crypto from "crypto";
 import { PostType } from "../../../type";
 
 /*export const config = {
@@ -11,33 +9,18 @@ import { PostType } from "../../../type";
     }
 }*/
 
-let fileName = "";
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: "./public",
-    filename: (_, file, cb) => {
-      fileName = `${crypto.randomUUID()}-${file.originalname}`;
-      cb(null, fileName);
-    },
-  }),
-});
 
 const handler = nc({
   onError(err, _, res: NextApiResponse) {
     res.status(400).json({ msg: "Something Went Wrong.." });
   },
-}).use(upload.single("file"));
+})
 
 handler.get(async (_, res: NextApiResponse) => {
   const { db } = await connectToDatabase();
   const Post = db.collection<PostType>("Post");
   const Posts = await Post.find({}, { projection: { _id: 0 } }).toArray();
   res.json({ post: Posts });
-});
-
-handler.post(async (_, res: NextApiResponse) => {
-  res.json({ msg: fileName });
 });
 
 export default handler;
