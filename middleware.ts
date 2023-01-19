f7610fd7457d7e4fb24edfe4c4f2921d9e7dfa55
petersahanaya <petersahanaya09@gmail.com>
2023-01-19 19:24:25 +0900

    .

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,17 @@
-export { default } from "next-auth/middleware";
+import { NextMiddleware, NextRequest, NextResponse } from "next/server";
+
+const middleware : NextMiddleware = async (req : NextRequest) => {
+    const token = req.cookies.get("__Secure-next-auth.session-token")?.value
+
+    if(!token) {
+      return NextResponse.redirect(new URL("https://p3social.vercel.app"))
+    }
+
+    return NextResponse.next()
+}
+
+export default middleware
+
 export const config = {
   matcher: [
     "/profile/:path*",
